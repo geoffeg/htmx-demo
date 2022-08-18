3af12fe294f01ca1b946e6cfd46fd7dec7674729
Geoffrey Gallaway <github@geoffeg.org>
2022-08-18 00:09:30 +0000

    Stuff

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ const bodyParser = require('body-parser');
 
 const app = express();
 app.use(express.static('public'));
-app.set('views', './partials');
+app.set('views', './views');
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
 app.listen(8080, () => {
-    console.log(`Listening on port 8080`);
+    console.log('Listening on port 8080');
 })
